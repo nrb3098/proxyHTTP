7f9066c74e52827428911b66fc754ff91d185da9
nicol <nicolas-ramos@javeriana.edu.co>
2021-10-21 16:36:18 -0500

    Añado envio de request

--- src/ManejadorCliente.java
+++ src/ManejadorCliente.java
@@ -1,12 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.Socket;
+import java.net.*;
+
 
 public class ManejadorCliente {
     public static void manejarCliente(Socket client) throws IOException {
         System.out.println("TENEMOS CLIENTE NUEVO: " + client.toString());
         BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        URL url = new URL("http://www.columbia.edu/~fdc/sample.html");
+        HttpURLConnection conexion = (HttpURLConnection) url.openConnection();
+        conexion.setRequestMethod("GET");
 
         StringBuilder requestBuilder = new StringBuilder();
         String line;
@@ -17,5 +21,11 @@ public class ManejadorCliente {
         String request = requestBuilder.toString();
         System.out.println("SE SOLICITA ESTO: \n");
         System.out.println(request);
+        System.out.println("SE entregó ESTO: \n");
+        System.out.println(conexion.getResponseCode());
     }
+    public void enviarRequest(String request){
+
+    }
+
 }
