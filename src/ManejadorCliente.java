1551b8dafb865e35110bbce37a75dd5918730d83
nicolas <nicolas-ramos@javeriana.edu.co>
2021-10-23 21:15:36 -0500

    Se añade el manejo de cadenas y el pedido y reenvío

--- src/ManejadorCliente.java
+++ src/ManejadorCliente.java
@@ -1,32 +1,75 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.*;
+//import java.util.Map;
 
 
 public class ManejadorCliente {
-    public static void manejarCliente(Socket client) throws IOException {
-        System.out.println("TENEMOS CLIENTE NUEVO: " + client.toString());
-        BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        //URL url = new URL("http://www.columbia.edu/~fdc/sample.html");
-        //HttpURLConnection conexion = (HttpURLConnection) url.openConnection();
-        //conexion.setRequestMethod("GET");
-
+    public static void manejarCliente(Socket clientEntrada) throws IOException {
+        System.out.println("TENEMOS CLIENTE NUEVO: " + clientEntrada.toString());
+        BufferedReader br = new BufferedReader(new InputStreamReader(clientEntrada.getInputStream()));                       
         StringBuilder requestBuilder = new StringBuilder();
         String line;
-        while (!(line = br.readLine()).isBlank()) {
-            requestBuilder.append(line + "\r\n");
+        System.out.println("SE SOLICITA ESTO: \n");
+        while ((line = br.readLine())!=null) {
+            requestBuilder.append(line + "\r\n");   
         }
 
+        
+        //sacamos el metodo y la url para abrir
         String request = requestBuilder.toString();
         System.out.println("SE SOLICITA ESTO: \n");
-        System.out.println(request);
-         
-        //System.out.println("SE RESPONDIENDO ESTO: \n");
-        //System.out.println(conexion.getResponseCode());
+        //String direccion = urlDeSolicitud(request);
+        //String metodosolicitud = metodoDeSolicitud(request);
+        
+        System.out.println(request);              
+        
+        /*
+        if(direccion!=null && metodosolicitud!=null){
+            URL url = new URL(direccion);
+            HttpURLConnection conexion= (HttpURLConnection) url.openConnection();            
+            conexion.setRequestMethod(metodosolicitud);
+            System.out.println("SE RESPONDIENDO ESTO: \n");
+            System.out.println(conexion.getResponseCode() + " " + conexion.getResponseMessage());
+            BufferedReader bufferEntrada = new BufferedReader(new InputStreamReader(conexion.getInputStream()));                                   
+            OutputStream clientOutput = clientEntrada.getOutputStream();
+            String i;   
+            while ((i = bufferEntrada.readLine()) != null)    
+            {   
+                System.out.println(i);
+                clientOutput.write(i.getBytes());       
+            }             
+        }*/
+        
+        
+        
     }
-    public void enviarRequest(String request){
 
+
+    public static String metodoDeSolicitud(String request){
+        int metodoini = request.indexOf("GET");
+        int metofin = request.indexOf(" h");
+
+        if(metodoini != -1 && metofin != -1){
+            String metodo = request.substring(metodoini,metofin);
+            return metodo;
+        }
+        return null;
+    }
+
+
+    public static String urlDeSolicitud(String request){
+        int inicioURl =  request.indexOf("http:");        
+        int finalURL = request.indexOf(" H");
+
+        if (inicioURl != -1 && finalURL != -1){
+            String direccion = request.substring(inicioURl, finalURL);
+            return direccion;
+        }
+        return null;
+        
     }
 
 }
